40764abc651424690d535bff053d4bd445c4feee
Daniel <danielgama1@live.com>
2023-10-20 00:26:55 -0300

    Criação do Projeto de uma API

---
+++ src/test/java/Gama/Daniel/dgamaprojectApiRest/DgamaProjectApiRestApplicationTests.java
@@ -0,0 +1,13 @@
+package Gama.Daniel.dgamaprojectApiRest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DgamaProjectApiRestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
